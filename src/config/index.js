df02f8917f4c98a81d6e5ce650c61a704bca88ea
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-09-13 07:46:08 +0200

    init project

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+export default {
+  "port": 3005,
+  "mongoUrl": "mongodb://localhost:2017/restaurant-api"
+};
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import config from '../config';
+import middleware from '../middleware';
+import initializeDb from '../db';
+
+let router = express();
+
+// connect to db
+initializeDb(db => {
+
+  // internal middlewares
+  router.use(middleware({ config, db }));
+
+  // api routes va (/v1)
+
+});
+
+export default router;
